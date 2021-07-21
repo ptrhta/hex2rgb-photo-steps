db27116380a1e5e4943dead5ca7c30ed274e6efa
ptrhta <49519262+ptrhta@users.noreply.github.com>
2021-07-21 19:16:36 +0300

    add steps

---
+++ steps/src/components/Steps/Steps.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import uuid from 'react-uuid'
+import Table from '../Table/Table';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import './steps.css';
+
+export default function Steps() {
+
+    const [ currentData, setCurrentData ] = useState({
+        date: '',
+        km: '',
+      }
+    );
+    const [tableData, setTableData ] = useState([]);
+    const [editMode, setEditMode] = useState(false);
+    const [editModeData, setEditModeData] = useState({});
+
+    const formatDate = date => {
+      return moment(date).format('DD.MM.YYYY')
+    }
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+
+        setCurrentData(prevData => {
+          return {
+            ...prevData,
+            [name]: value,
+          }
+        })
+    }
+
+    const handleChangeDate = date => {
+        const newDate = date ? date: currentData.date;
+
+        setCurrentData(prevData => {
+          return {
+            ...prevData,
+            'date': newDate
+          }
+        })
+    }
+
+    const handleDelete = (e, id) => {
+      setTableData(prevData => {
+        return prevData.filter(row => row.id !== id);
+      })
+    }
+
+    const handleEdit = (e, id) => {
+      e.preventDefault();
+      if (!editMode) {
+        setEditModeData(tableData.filter(row => row.id === id)[0]);
+      }
+      setEditMode(prevEditMode => !prevEditMode);
+    }
+
+    const handleSubmit = (e, currentData) => {
+        e.preventDefault();
+
+        setTableData(data => {
+          let isNewDate = true;
+            let newDate = data.map(step => {
+                if (formatDate(step.date) === formatDate(currentData.date)) {
+                  isNewDate = false;
+                  if (editMode) {
+                    return {
+                        ...step, date: currentData.date, km: +currentData.km
+                      }
+                  }
+                    return {
+                        ...step, id: uuid(), date: currentData.date, km: +currentData.km + +step.km
+                    }
+                } else {
+                  return {
+                    ...step
+                  }
+                }
+            })
+
+            if (!isNewDate) return newDate
+
+            return [...data, {...currentData, id: uuid()}]
+        })
+      };
+
+    return(
+        <div className="steps">
+            <form className="steps-form" onSubmit={(e) => handleSubmit(e, currentData)}>
+               <div className="col">
+                  <label htmlFor='date'>Дата (ДД.ММ.ГГГГ)</label>
+                  <DatePicker 
+                    dateFormat="dd.MM.yyyy" 
+                    selected={currentData.date} 
+                    onChange={handleChangeDate} 
+                  />
+                </div>
+                <div className="col">
+                  <label htmlFor='km'>Пройдено, км</label>
+                  <input 
+                      id='km' 
+                      name='km' 
+                      type="number"
+                      className={`${editMode ? 'editMode' : ''}`}
+                      value={currentData.km} 
+                      onChange={handleChange}
+                  />
+                </div>
+                <button type="submit" className="btnSubmit">ОК</button>
+            </form>
+            <Table 
+                data={tableData} 
+                handleDelete={handleDelete}
+                handleEdit={handleEdit}
+                editModeData={editModeData}
+                setCurrentData={setCurrentData}
+            />
+        </div>
+    )
+}
---
+++ steps/src/components/Table/Table.jsx
@@ -0,0 +1,83 @@
+import React, {useEffect} from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
+import moment from 'moment';
+
+import './table.css'
+
+
+export default function Table(props) {
+    const data = props.data;
+    const handleDelete = props.handleDelete;
+    const editModeData = props.editModeData;
+    const setCurrentData = props.setCurrentData;
+    const handleEdit = props.handleEdit;
+
+    useEffect(() => {
+      if (Object.keys(editModeData).length) {
+        setCurrentData({
+          date: editModeData.date,
+          km: editModeData.km,
+        })
+      }
+    }, [editModeData]);
+
+    const formatDate = date => {
+      return moment(date).format('DD.MM.YYYY')
+    }
+
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                <th>Дата (ДД.ММ.ГГГГ)</th>
+                <th>Пройдено, км</th>
+                <th>Действия</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    data.sort((a, b) => a.date - b.date)
+                    .map(step => {
+                        return(
+                          <tr key={step.id}>
+                            <td>
+                              <p>{formatDate(step.date)}</p>
+                            </td>
+                            <td>
+                              <p>{step.km}</p>
+                            </td>
+                            <td>
+                              <button className="edit" onClick={e => handleEdit(e, step.id)}>
+                                <FontAwesomeIcon icon={faEdit} />
+                              </button>
+                              <button className="delete" onClick={e => handleDelete(e, step.id)}>
+                                <FontAwesomeIcon icon={faTrashAlt} />
+                              </button>
+                            </td>
+                          </tr>
+                        )
+                })
+            }
+            </tbody>
+        </table>
+    )
+}
+
+Table.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.string.isRequired,
+      km: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+      ]).isRequired,
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  ),
+  handleDelete: PropTypes.func.isRequired, 
+  handleEdit: PropTypes.func.isRequired,
+  setCurrentData: PropTypes.func.isRequired,
+  editModeData: PropTypes.object.isRequired
+};
